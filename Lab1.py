8d16eb34b6fe4220c65096fd8be49dab3ef6cf51
Ahub0000 <alec.moskos03@gmail.com>
2026-02-11 01:29:59 -0800

    Initial commit

---
+++ Lab1.py
@@ -0,0 +1,8 @@
+# Filename: Lab1
+# Author Alec M
+# Created: 2026-02-09
+# Description: This program stores a message in
+# in a variable and prints it in the terminal. 
+
+message = "Go gators!"
+print(message)
